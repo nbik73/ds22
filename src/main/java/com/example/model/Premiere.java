c695675c9306af9d9103a2aeb9e65015ab755526
Nbik73 <nbik73@yandex.ru>
2022-04-01 20:29:52 +0300

    Inital commit

---
+++ src/main/java/com/example/model/Premiere.java
@@ -0,0 +1,46 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Premiere {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String nameOfThePremiere; //название
+    private String description;       //описание премьеры
+    private Integer ageCategory;          //возрастная категория
+    private Integer quantityOfSeats;      //количество мест
+
+    public Premiere(String nameOfThePremiere, String description, int ageCategory, int quantityOfSeats) {
+        this.nameOfThePremiere = nameOfThePremiere;
+        this.description = description;
+        this.ageCategory = ageCategory;
+        this.quantityOfSeats = quantityOfSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "Премьера {" +
+                "" + nameOfThePremiere + '\'' +
+                ", description='" + description + '\'' +
+                ", ageCategory=" + ageCategory +
+                ", seatList=" + quantityOfSeats +
+                '}';
+    }
+}
